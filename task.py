84a4371a5a9fc56390c80a756e359959f448c964
Vadim Colcev <vadimkolchev@gmail.com>
2021-07-01 13:38:10 +0300

    refactoring

--- task.py
+++ task.py
@@ -9,9 +9,9 @@ class Task:
     def __init__(self, name):
         self.name = name
         self.is_done = False
-        self.id = self.total_tasks + 1
-        self.date = datetime.now(self.chisinau)
         self.update_total()
+        self.id = self.total_tasks
+        self.date = datetime.now(self.chisinau)
 
     def __str__(self):
         return f"""
